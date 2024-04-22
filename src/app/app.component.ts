599e8d2d826e4219877d71a006afd289d255716f
Philip Küchler <philip.kuechler@web.de>
2024-04-22 22:43:25 +0200

    first build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ToolbarModule} from "primeng/toolbar";
+import {ButtonModule} from "primeng/button";
+import {MenuModule} from "primeng/menu";
+import {MegaMenuItem, MenuItem} from "primeng/api";
+import {TabMenuModule} from "primeng/tabmenu";
+import {MegaMenuModule} from "primeng/megamenu";
+import {PanelMenuModule} from "primeng/panelmenu";
 
 @Component({
-  selector: 'pk-root',
+  selector: 'lp-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToolbarModule, ButtonModule, MenuModule, TabMenuModule, MegaMenuModule, PanelMenuModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'landingpage';
+export class AppComponent implements OnInit {
+  title = "landingpage";
+  items: MegaMenuItem[] = [
+    {
+      label: 'Über mich',
+      icon: '',
+    },
+    {
+      label: 'Erfahrungen',
+      icon: ''
+    },
+    {
+      label: 'Skills',
+      icon: ''
+    },
+    {
+      label: 'Zertifikate',
+      icon: ''
+    },
+    {
+      label: 'Erfolge',
+      icon: ''
+    }
+  ];
+
+  ngOnInit() {
+
+  }
 }
