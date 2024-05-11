60fabcf046c05c67080f0668515acc32ae62a79e
Philip Küchler <philip.kuechler@web.de>
2024-05-11 14:37:47 +0200

    active route style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import {Router, RouterOutlet} from '@angular/router';
 import {ToolbarModule} from "primeng/toolbar";
 import {ButtonModule} from "primeng/button";
 import {MenuModule} from "primeng/menu";
@@ -7,7 +7,7 @@ import {MegaMenuItem} from "primeng/api";
 import {TabMenuModule} from "primeng/tabmenu";
 import {MegaMenuModule} from "primeng/megamenu";
 import {PanelMenuModule} from "primeng/panelmenu";
-import {DOCUMENT} from "@angular/common";
+import {DOCUMENT, NgClass, NgIf} from "@angular/common";
 import {ToggleButtonModule} from "primeng/togglebutton";
 import {FormsModule} from "@angular/forms";
 import {BackgroundComponent} from "./background/background.component";
@@ -15,7 +15,7 @@ import {BackgroundComponent} from "./background/background.component";
 @Component({
   selector: 'lp-root',
   standalone: true,
-  imports: [RouterOutlet, ToolbarModule, ButtonModule, MenuModule, TabMenuModule, MegaMenuModule, PanelMenuModule, ToggleButtonModule, FormsModule, BackgroundComponent],
+  imports: [RouterOutlet, ToolbarModule, ButtonModule, MenuModule, TabMenuModule, MegaMenuModule, PanelMenuModule, ToggleButtonModule, FormsModule, BackgroundComponent, NgIf, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -26,25 +26,30 @@ export class AppComponent implements OnInit {
       label: 'Über mich',
       icon: 'pi pi-id-card',
       routerLink: '/about-me',
+      routerLinkActiveOptions: {exact: true},
+      state: [""]
     },
     {
       label: 'Erfahrung',
       icon: 'pi pi-clock',
-      routerLink: '/experience'
+      routerLink: '/experience',
+      routerLinkActiveOptions: {exact: true}
     },
     {
       label: 'Skills',
       icon: 'pi pi-star',
-      routerLink: '/skills'
+      routerLink: '/skills',
+      routerLinkActiveOptions: {exact: true}
     },
     {
       label: 'Zertifikate',
       icon: 'pi pi-verified',
-      routerLink: '/certificates'
+      routerLink: '/certificates',
+      routerLinkActiveOptions: {exact: true}
     }
   ];
 
-  constructor(@Inject(DOCUMENT) private document: Document) {
+  constructor(@Inject(DOCUMENT) private document: Document, private router: Router) {
   }
 
   ngOnInit() {
@@ -56,5 +61,10 @@ export class AppComponent implements OnInit {
     if (themeLink) {
       themeLink.href = "assets/themes/" + (this.themeToggled ? "lara-light-blue/theme.css" : "lara-dark-blue/theme.css");
     }
+  }
+
+  checkRouteActive(route: string) {
+    console.log(this.router.url)
+    return route === this.router.url;
   }
 }
